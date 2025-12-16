4e660050b5efe84f965101a4c397e473d2aae172
ibeh christopher <109024548+gambinoxxx@users.noreply.github.com>
2025-12-16 13:10:57 +0100

    Reorder Work component in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
       <Services />
      <CoreFeatures />
       <QualityFeatures />
-      <Work />
       <Team />
+      <Work />
       <Testimonials />
       <Subscribe />
     </div>
